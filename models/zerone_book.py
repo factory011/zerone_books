cf0fd8e65a53733eda6b71f2758810b2d96d6bde
Mark Wu <thurendous2@gmail.com>
2020-06-23 14:15:25 +0900

    minor change 6/23

--- models/zerone_book.py
+++ models/zerone_book.py
@@ -13,7 +13,7 @@ class ZeroneBook(models.Model):
     isbn = fields.Char(string="ISBN", copy=False)
     author = fields.Char(string="作者")
     pages = fields.Integer(string="ページ数")
-    publish_date = fields.Date(string="出版时间")
+    publish_date = fields.Date(string="出版時期")
     publisher = fields.Char(string="出版社")
     price = fields.Float(string="定価", digits=(7, 2))
     description = fields.Text(string="概要", help="""本図書の概要説明""")
@@ -23,7 +23,7 @@ class ZeroneBook(models.Model):
     )
     e_link = fields.Html(string="電子版リンク")
     borrowed = fields.Boolean(string="貸出中", default=False)
-    date_last_borrowed = fields.Datetime("最後の貸出時刻", index=True, readonly=True)
+    date_last_borrowed = fields.Datetime("最後の貸出時刻", index=True)
 
     shelf_id = fields.Many2one('zerone.shelf', string='書架')
     tags_ids = fields.Many2many("zerone.tags", string="タグ")
